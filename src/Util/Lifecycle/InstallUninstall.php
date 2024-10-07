1121cd9c41c9b581f19a1d49f13dc5404e7236ec
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2024-10-07 12:57:37 +0200

    prevent script duplication in sw < 6.6

--- src/Util/Lifecycle/InstallUninstall.php
+++ src/Util/Lifecycle/InstallUninstall.php
@@ -71,6 +71,7 @@ class InstallUninstall
     {
         $this->addDefaultConfiguration($lifecycleContext);
         $this->addPaymentMethods($lifecycleContext->getContext());
+        $this->fixLegacyScriptLocation($lifecycleContext->getCurrentShopwareVersion());
     }
 
     public function uninstall(UninstallContext $lifecycleContext): void
@@ -81,6 +82,7 @@ class InstallUninstall
     public function update(UpdateContext $lifecycleContext): void
     {
         $this->addDefaultConfiguration($lifecycleContext);
+        $this->fixLegacyScriptLocation($lifecycleContext->getCurrentShopwareVersion());
     }
 
     private function addDefaultConfiguration($lifecycleContext): void
@@ -173,6 +175,24 @@ class InstallUninstall
         }
 
         $this->paymentMethodRepository->upsert([$data], $context);
+    }
+
+    protected function fixLegacyScriptLocation($swVersion) {
+        if (\version_compare($swVersion, '6.6.0', '>=')) {
+            return;
+        }
+
+        $pluginDir = \dirname(__FILE__).'/../..';
+        $scriptsDir = $pluginDir . '/Resources/app/storefront/dist/storefront/js';
+        $legacyFilePath = $scriptsDir . '/easy-credit-ratenkauf.js';
+        $newFilePath = $scriptsDir . '/easy-credit-ratenkauf/easy-credit-ratenkauf.js';
+        if (\file_exists($newFilePath)) {
+            if (!\file_exists($legacyFilePath)) {
+              \rename($newFilePath, $legacyFilePath); // move the shipped file to legacy location
+            } else {
+              \unlink($newFilePath); // delete the >= SW6.6 file to prevent duplication
+            }
+        }
     }
 
     protected function getCountryIds(array $countryIsos, Context $context): array
